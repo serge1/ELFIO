0b72c19280c394a78a394c6eae0a81442d00d4fc
Serge Lamikhov-Center <to_serge@users.sourceforge.net>
2012-02-18 19:04:57 +0200

    Web files added to the repository

---
+++ web/htdocs/index.php
@@ -0,0 +1,165 @@
+<HTML
+><HEAD
+><TITLE
+>ELFIO</TITLE
+><META
+NAME="GENERATOR"
+CONTENT="Modular DocBook HTML Stylesheet Version 1.74b"><LINK
+REL="NEXT"
+TITLE="Introduction"
+HREF="f14.htm"></HEAD
+><BODY
+CLASS="BOOK"
+BGCOLOR="#FFFFFF"
+TEXT="#000000"
+LINK="#0000FF"
+VLINK="#840084"
+ALINK="#0000FF"
+><DIV
+CLASS="BOOK"
+><A
+NAME="AEN1"
+>
+<div align="right"><A href="http://sourceforge.net"><IMG src="http://sourceforge.net/sflogo.php?group_id=19959&type=2" width="125" height="37" border="0" alt="SourceForge Logo"></A></div>
+</A
+><DIV
+CLASS="TITLEPAGE"
+><H1
+CLASS="TITLE"
+><A
+NAME="AEN2"
+>ELFIO</A
+></H1
+><H2
+CLASS="SUBTITLE"
+>Tutorial</H2
+>      <H3
+CLASS="AUTHOR"
+><A
+NAME="AEN7"
+>Allan Finch</A
+></H3
+>
+      <H3
+CLASS="AUTHOR"
+><A
+NAME="AEN10"
+>Serge Lamikhov-Center</A
+></H3
+>
+    <HR></DIV
+><DIV
+CLASS="TOC"
+><DL
+><DT
+><B
+>Table of Contents</B
+></DT
+><DT
+><A
+HREF="f14.htm"
+>Introduction</A
+></DT
+><DT
+>1. <A
+HREF="c18.htm"
+>Getting Started With ELFIO</A
+></DT
+><DD
+><DL
+><DT
+><A
+HREF="c18.htm#AEN20"
+>Initialization</A
+></DT
+><DT
+><A
+HREF="x35.htm"
+>ELF File Sections</A
+></DT
+><DT
+><A
+HREF="x45.htm"
+>Section Readers</A
+></DT
+><DT
+><A
+HREF="x57.htm"
+>Finalization</A
+></DT
+></DL
+></DD
+><DT
+>2. <A
+HREF="c63.htm"
+>ELFDump Utility</A
+></DT
+><DT
+>3. <A
+HREF="c66.htm"
+><FONT
+COLOR="RED"
+>IELFO</FONT
+> - ELF File Producer Interface</A
+></DT
+></DL
+></DIV
+>
+
+
+
+
+  </DIV
+><DIV
+CLASS="NAVFOOTER"
+><HR
+ALIGN="LEFT"
+WIDTH="100%"><TABLE
+SUMMARY="Footer navigation table"
+WIDTH="100%"
+BORDER="0"
+CELLPADDING="0"
+CELLSPACING="0"
+><TR
+><TD
+WIDTH="33%"
+ALIGN="left"
+VALIGN="top"
+>&nbsp;</TD
+><TD
+WIDTH="34%"
+ALIGN="center"
+VALIGN="top"
+>&nbsp;</TD
+><TD
+WIDTH="33%"
+ALIGN="right"
+VALIGN="top"
+><A
+HREF="f14.htm"
+ACCESSKEY="N"
+>Next</A
+></TD
+></TR
+><TR
+><TD
+WIDTH="33%"
+ALIGN="left"
+VALIGN="top"
+>&nbsp;</TD
+><TD
+WIDTH="34%"
+ALIGN="center"
+VALIGN="top"
+>&nbsp;</TD
+><TD
+WIDTH="33%"
+ALIGN="right"
+VALIGN="top"
+>Introduction</TD
+></TR
+></TABLE
+></DIV
+></BODY
+></HTML
+>
