9e4502d90eae0fbef96e32dc89dfac44f3e739eb
Serge Lamikhov-Center <to_serge@users.sourceforge.net>
2015-02-13 20:51:07 +0200

    Copyright year update

--- examples/elfdump/elfdump.cpp
+++ examples/elfdump/elfdump.cpp
@@ -1,7 +1,7 @@
 /*
-ELFDump.cpp - Dump ELF file using ELFIO library.
+elfdump.cpp - Dump ELF file using ELFIO library.
 
-Copyright (C) 2001-2011 by Serge Lamikhov-Center
+Copyright (C) 2001-2015 by Serge Lamikhov-Center
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
