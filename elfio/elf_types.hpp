3c92b26c32e00ef98d6baf94ac564c0b3d5d453f
Gleb Struchalin <glebx-f@mail.ru>
2019-04-17 22:07:55 +0300

    Added parentheses in ELF64_R_INFO macro for 's' parameter

--- elfio/elf_types.hpp
+++ elfio/elf_types.hpp
@@ -827,7 +827,7 @@ struct Elf64_Rela {
 
 #define ELF64_R_SYM(i)    ((i)>>32)
 #define ELF64_R_TYPE(i)   ((i)&0xffffffffL)
-#define ELF64_R_INFO(s,t) ((((int64_t)s)<<32)+((t)&0xffffffffL))
+#define ELF64_R_INFO(s,t) ((((int64_t)(s))<<32)+((t)&0xffffffffL))
 
 // Dynamic structure
 struct Elf32_Dyn {
