70ad1ae727ed31fc88e272a01abe3f3f4fd306cf
Serge Lamikhov-Center <to_serge@users.sourceforge.net>
2020-08-07 23:04:17 +0300

    Use header only version of the Boost Test library

--- ELFIOTest/ELFIOTest.cpp
+++ ELFIOTest/ELFIOTest.cpp
@@ -2,12 +2,8 @@
 #define _SCL_SECURE_NO_WARNINGS
 #endif
 
-#define BOOST_TEST_MAIN
-#ifndef _MSC_VER
-#define BOOST_TEST_DYN_LINK
-#endif
 #define BOOST_TEST_MODULE ELFIO_Test
-#include <boost/test/unit_test.hpp>
+#include <boost/test/included/unit_test.hpp>
 
 #include <elfio/elfio.hpp>
 
